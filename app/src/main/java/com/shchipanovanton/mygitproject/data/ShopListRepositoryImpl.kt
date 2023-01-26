bdcbd0bbc42d778c6880fd582ed81c51049bba1e
toxa559 <toxa559@gmail.com>
2023-01-26 12:00:38 +0300

    add data class

---
+++ app/src/main/java/com/shchipanovanton/mygitproject/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.shchipanovanton.mygitproject.data
+
+import com.shchipanovanton.mygitproject.domain.ShopItem
+import com.shchipanovanton.mygitproject.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId =0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID){
+        shopItem.id = autoIncrementId
+        autoIncrementId++}
+
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItemUseCase(shopItem: ShopItem) {
+       val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+      return  shopList.find { it.id == shopItemId
+      } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+
+}
--- app/src/main/java/com/shchipanovanton/mygitproject/domain/ShopItem.kt
+++ app/src/main/java/com/shchipanovanton/mygitproject/domain/ShopItem.kt
@@ -1,11 +1,16 @@
 package com.shchipanovanton.mygitproject.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count : Int,
-    val enabled : Boolean
-)
+    val enabled : Boolean,
+    var id: Int = UNDEFINED_ID
+)  {
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+
+}
 
 
 
